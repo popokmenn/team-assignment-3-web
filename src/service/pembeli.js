7b385f6be64ff35c9daf592636cbcd96fd467619
Naufal Ajisasongko <Naufal.Ajisasongko>
2022-10-30 19:40:50 +0700

    change base url

--- src/service/pembeli.js
+++ src/service/pembeli.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = 'http://127.0.0.1:8000'
-// const BASE_URL = 'https://tk2-server.herokuapp.com'
+// const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = 'https://tk2-server.herokuapp.com'
 
 
 export async function _getPembeli() {
